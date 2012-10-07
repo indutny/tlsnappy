10dedcf5ae0148fc51ef8b970c84c662c4087220
Fedor Indutny <fedor.indutny@gmail.com>
2012-10-07 20:39:09 +0700

    tlsnappy: moar OpenSSL options

--- src/tlsnappy.cc
+++ src/tlsnappy.cc
@@ -31,6 +31,8 @@ Context::Context() : status_(kRunning), npn_(NULL) {
   // Mitigate BEAST attacks
   SSL_CTX_set_options(ctx_, SSL_OP_CIPHER_SERVER_PREFERENCE);
   SSL_CTX_set_options(ctx_, SSL_OP_NO_COMPRESSION);
+  SSL_CTX_set_options(ctx_, SSL_OP_SINGLE_DH_USE);
+  SSL_CTX_set_mode(ctx_, SSL_MODE_RELEASE_BUFFERS);
   SSL_CTX_set_session_cache_mode(ctx_, SSL_SESS_CACHE_OFF);
   SSL_CTX_set_read_ahead(ctx_, 1);
 
