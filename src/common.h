90405054bfcd1bb17907d4960c8bf8a52e13473e
Bert Belder <bertbelder@gmail.com>
2012-10-10 16:45:09 +0200

    windows: fix compilation

--- src/common.h
+++ src/common.h
@@ -1,7 +1,14 @@
 #ifndef _SRC_COMMON_H_
 #define _SRC_COMMON_H_
 
+#include <stdint.h>
 #include <stdlib.h>
+
+#if defined(_WIN32) && !defined(_SSIZE_T_) && !defined(_SSIZE_T_DEFINED)
+  typedef intptr_t ssize_t;
+# define _SSIZE_T_
+# define _SSIZE_T_DEFINED
+#endif
 
 #ifndef offset_of
 // g++ in strict mode complains loudly about the system offsetof() macro
